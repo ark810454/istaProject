9c905ee5b6f8b8cd5ad97f3b4f26ee42335e5acb
ark810454 <ark810454@gmail.com>
2024-05-11 12:22:33 +0100

    chrisadd

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 
-let viewToolTip = document.getElementById("viewToolTip")
+let viewToolTip = document.querySelectorAll("#viewToolTip")
 let tooltipAcount = document.getElementById("items")
+let caret= document.querySelectorAll("#caretbtn");
 var rememberState = false
-
+for (let i=0; i<viewToolTip.length; i++){
 window.addEventListener('click', function(){
 
   rememberState = false
   if(rememberState === false){
     tooltipAcount.style.display = "none";
-document.getElementById("caretbtn").style.transform = "rotate(0deg)"
-document.getElementById("caretbtn").style.transition = "0.2s transform ease-in-out" 
+caret[i].style.transform = "rotate(0deg)"
+caret[i].style.transition = "0.2s transform ease-in-out" 
   }
 })
+
 
 tooltipAcount.addEventListener('click', function(e){
   e.stopPropagation();
@@ -19,79 +21,49 @@ tooltipAcount.addEventListener('click', function(e){
   if(rememberState === true){
     tooltipAcount.style.display="flex";
 
-document.getElementById("caretbtn").style.transform = "rotate(180deg)"
-document.getElementById("caretbtn").style.transition = "0.2s transform ease-in-out" 
+caret[i].style.transform = "rotate(180deg)"
+caret[i].style.transition = "0.2s transform ease-in-out" 
   }
 })
-viewToolTip.addEventListener('click', function(e){
+viewToolTip[i].addEventListener('click', function(e){
   e.stopPropagation();
   rememberState = !rememberState
   if(rememberState === true){
     tooltipAcount.style.display="flex";
-document.getElementById("caretbtn").style.transform = "rotate(180deg)"
-document.getElementById("caretbtn").style.transition = "0.2s transform ease-in-out" 
+caret[i].style.transform = "rotate(180deg)"
+caret[i].style.transition = "0.2s transform ease-in-out" 
   }
   else{
     tooltipAcount.style.display = "none";
-document.getElementById("caretbtn").style.transform = "rotate(0deg)"
-document.getElementById("caretbtn").style.transition = "0.2s transform ease-in-out" 
+caret[i].style.transform = "rotate(0deg)";
+caret[i].style.transition = "0.2s transform ease-in-out" ;
   }
   
 
 
 })
+}
 
-const link1 = document.getElementById('link1');
-const items1 = document.getElementById('items1');
-const caretbottom = document.querySelector('.caret_bottom')
-link1.addEventListener('mouseover', function() {
-    items1.style.display = 'flex';
-    caretbottom.style.transform = "rotate(180deg)"
-caretbottom.style.transition = "0.2s transform ease-in-out" 
+const link1 = document.querySelectorAll('#link1');
+const items1 = document.querySelectorAll('#items1');
+const caretbottom = document.querySelectorAll('.caret_bottom');
+for(let i =0; i<link1.length; i++){
+link1[i].addEventListener('mouseover', function() {
+    items1[i].style.display = 'flex';
+    caretbottom[i].style.transform = "rotate(180deg)"
+caretbottom[i].style.transition = "0.2s transform ease-in-out" 
+console.log(i)
 });
 
-link1.addEventListener('mouseleave', function() {
-  if (!items1.matches(':hover')) {
-    items1.style.display = 'none';
-    caretbottom.style.transform = "rotate(0deg)"
-caretbottom.style.transition = "0.2s transform ease-in-out" 
+link1[i].addEventListener('mouseleave', function() {
+  if (!items1[i].matches(':hover')) {
+    items1[i].style.display = 'none';
+    caretbottom[i].style.transform = "rotate(0deg)"
+caretbottom[i].style.transition = "0.2s transform ease-in-out" 
   }
-});
+});}
 
 
-const link2 = document.getElementById('link2');
-const items2 = document.getElementById('items2');
-const caretbottom2 = document.querySelector('.caret_bottom2')
-link2.addEventListener('mouseover', function() {
-    items2.style.display = 'flex';
-    caretbottom2.style.transform = "rotate(180deg)"
-caretbottom2.style.transition = "0.2s transform ease-in-out" 
-});
-
-link2.addEventListener('mouseleave', function() {
-  if (!items2.matches(':hover')) {
-    items2.style.display = 'none';
-    caretbottom2.style.transform = "rotate(0deg)"
-caretbottom2.style.transition = "0.2s transform ease-in-out" 
-  }
-});
-
-const link3 = document.getElementById('link3');
-const items3 = document.getElementById('items3');
-const caretbottom3 = document.querySelector('.caret_bottom3')
-link3.addEventListener('mouseover', function() {
-    items3.style.display = 'flex';
-    caretbottom3.style.transform = "rotate(180deg)"
-caretbottom3.style.transition = "0.2s transform ease-in-out" 
-});
-
-link3.addEventListener('mouseleave', function() {
-  if (!items3.matches(':hover')) {
-    items3.style.display = 'none';
-    caretbottom3.style.transform = "rotate(0deg)"
-caretbottom3.style.transition = "0.2s transform ease-in-out" 
-  }
-});
 
 
 let allftMenu = document.querySelectorAll("#ftmenu");
